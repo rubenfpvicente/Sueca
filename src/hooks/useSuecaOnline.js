02fdd79267fb6d1819f6c4bcadad76cd9d72f79f
Ruben Vicente <2021116429@uac.pt>
2025-12-29 05:51:22 -0100

    Update

--- src/hooks/useSuecaOnline.js
+++ src/hooks/useSuecaOnline.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('https://sueca-server-production.up.railway.app/');
 
 const sortHand = (hand) => {
   const suitOrder = { 'C': 0, 'E': 1, 'O': 2, 'P': 3 };
